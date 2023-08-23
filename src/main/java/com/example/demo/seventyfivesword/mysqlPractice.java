cba07f7c483ecfc0c179e374807ef34fb0af1a7b
qinlida <qinlida@znlh.com>
2023-08-23 13:41:02 +0800

    [算法小练习]mysql基础练习

---
+++ src/main/java/com/example/demo/seventyfivesword/mysqlPractice.java
@@ -0,0 +1,20 @@
+package com.example.demo.seventyfivesword;
+
+public class mysqlPractice {
+    /**
+     * delete from user where ID= 1
+     * truncate table user
+     * alter table user add
+     * （修改字段描述类型或备注）modify id int(10)
+     * change  修改字段名 change 旧字段  新字段  类型（长度）
+     * drop 删除对应的字段 drop id
+     *
+     * constraint emp_dept_fk foreign key（user_id） reference department(id)
+     *
+     * alter table drop foreign key emp_dept_fk
+     *
+     * alter table add constraint emp_dept_fk foreign key（user_id） reference department(id)
+     *
+     *
+     */
+}
