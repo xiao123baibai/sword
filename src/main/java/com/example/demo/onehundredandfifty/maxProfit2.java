34001a85ac6bc13662e57b0d67df5c3b75e074af
qinlida <qinlida@znlh.com>
2023-09-18 17:28:49 +0800

    [面试经典150题]买卖股票的最佳时机含冷冻期

--- src/main/java/com/example/demo/onehundredandfifty/maxProfit2.java
+++ src/main/java/com/example/demo/onehundredandfifty/maxProfit2.java
@@ -51,7 +51,7 @@ public class maxProfit2 {
         dp[0][1] = -prices[0];
         for (int i = 1;i < prices.length;i++){
             dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);//cash
-            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]+prices[i]);//stock
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]-prices[i]);//stock
         }
         return dp[prices.length-1][0];
     }
