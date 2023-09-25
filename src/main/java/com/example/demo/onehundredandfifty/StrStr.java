ae5972a65c1d7bf5802c6d063ed274a0502922ac
qinlida <qinlida@znlh.com>
2023-09-25 10:11:15 +0800

    [面试经典150题]找出字符串中第一个匹配项的下标

---
+++ src/main/java/com/example/demo/onehundredandfifty/StrStr.java
@@ -0,0 +1,63 @@
+package com.example.demo.onehundredandfifty;
+
+import javax.swing.text.Position;
+
+/**
+ * 找出字符串中第一个匹配项的下标
+ */
+public class StrStr {
+    /**
+     * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle
+     * 字符串的第一个匹配项的下标（下标从 0 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：haystack = "sadbutsad", needle = "sad"
+     * 输出：0
+     * 解释："sad" 在下标 0 和 6 处匹配。
+     * 第一个匹配项的下标是 0 ，所以返回 0 。
+     * 示例 2：
+     *
+     * 输入：haystack = "leetcode", needle = "leeto"
+     * 输出：-1
+     * 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+     *
+     *
+     * 提示：
+     *
+     * 1 <= haystack.length, needle.length <= 104
+     * haystack 和 needle 仅由小写英文字符组成
+     */
+    //思路：暴力循环解法
+    public static int strStr(String haystack, String needle) {
+        boolean flag = haystack.contains(needle);
+        if (flag){
+            int temp = 0;
+            for (int i = 0; i < haystack.length();i++){
+                int position = i;
+                for (int j = 0; j < needle.length();j++){
+                    if (haystack.charAt(position) != needle.charAt(j)){
+                        temp = 0;
+                        break;
+                    }else {
+                        temp++;
+                    }
+                    position++;
+                    if (temp == needle.length()){
+                        return i;
+                    }
+                }
+
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "mississippi";
+        String needle = "issip";
+        strStr(haystack,needle);
+    }
+}
