e8e0778546ba4496076a377edd7001294e1b5fdf
qinlida <qinlida@znlh.com>
2023-11-02 11:01:02 +0800

    [面试经典150题]有效的数独

---
+++ src/main/java/com/example/demo/onehundredandfifty/IsValidSudoku.java
@@ -0,0 +1,133 @@
+package com.example.demo.onehundredandfifty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.bouncycastle.asn1.x500.style.RFC4519Style.c;
+
+/**
+ * 有效的数独
+ */
+public class IsValidSudoku {
+    /**
+     * 请你判断一个 9 x 9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+     *
+     * 数字 1-9 在每一行只能出现一次。
+     * 数字 1-9 在每一列只能出现一次。
+     * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+     *
+     *
+     * 注意：
+     *
+     * 一个有效的数独（部分已被填充）不一定是可解的。
+     * 只需要根据以上规则，验证已经填入的数字是否有效即可。
+     * 空白格用 '.' 表示。
+     *
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：board =
+     * [['5','3','.','.','7','.','.','.','.']
+     * ,['6','.','.','1','9','5','.','.','.']
+     * ,['.','9','8','.','.','.','.','6','.']
+     * ,['8','.','.','.','6','.','.','.','3']
+     * ,['4','.','.','8','.','3','.','.','1']
+     * ,['7','.','.','.','2','.','.','.','6']
+     * ,['.','6','.','.','.','.','2','8','.']
+     * ,['.','.','.','4','1','9','.','.','5']
+     * ,['.','.','.','.','8','.','.','7','9']]
+     * 输出：true
+     * 示例 2：
+     *
+     * 输入：board =
+     * [['8','3','.','.','7','.','.','.','.']
+     * ,['6','.','.','1','9','5','.','.','.']
+     * ,['.','9','8','.','.','.','.','6','.']
+     * ,['8','.','.','.','6','.','.','.','3']
+     * ,['4','.','.','8','.','3','.','.','1']
+     * ,['7','.','.','.','2','.','.','.','6']
+     * ,['.','6','.','.','.','.','2','8','.']
+     * ,['.','.','.','4','1','9','.','.','5']
+     * ,['.','.','.','.','8','.','.','7','9']]
+     * 输出：false
+     * 解释：除了第一行的第一个数字从 5 改为 8 以外，空格内其他数字均与 示例1 相同。 但由于位于左上角的 3x3 宫内有两个 8 存在, 因此这个数独是无效的。
+     *
+     *
+     * 提示：
+     *
+     * board.length == 9
+     * board[i].length == 9
+     * board[i][j] 是一位数字（1-9）或者 '.'
+     */
+    //思路：1、循环列 2，循环行 3、长度为3的数组验证
+    //个人通用做法，不局限于取巧
+    //使用数组更方便
+    public static boolean isValidSudoku(char[][] board) {
+        int rLen = board.length;
+        int cLen = board[0].length;
+
+        //行循环--双层循环
+        Map<Integer,Integer> rMap = new HashMap<>();
+        for (int i = 0; i < rLen;i++){
+            for (int j = 0;j < cLen;j++){
+                if (board[i][j]>'0' && board[i][j] <= '9' && rMap.getOrDefault(Integer.valueOf(board[i][j]),0) > 0){
+                    return false;
+                }
+                rMap.put(Integer.valueOf(board[i][j]), rMap.getOrDefault(Integer.valueOf(board[i][j]),0)+1);
+            }
+            rMap.clear();
+        }
+        //列循环--双层循环
+        Map<Integer,Integer> cMap = new HashMap<>();
+        for (int i = 0; i < cLen;i++){
+            for (int j = 0;j < rLen;j++){
+                if (board[j][i]>'0' && board[j][i] <= '9' && cMap.getOrDefault(Integer.valueOf(board[j][i]),0)>0){
+                    return false;
+                }
+                cMap.put(Integer.valueOf(board[j][i]), cMap.getOrDefault(Integer.valueOf(board[j][i]),0)+1);
+            }
+            cMap.clear();
+        }
+        //三位数组循环 双层循环
+        Map<Integer,Integer> threeMap = new HashMap<>();
+        int r = 0;
+        while (r <= rLen -3){
+            int c = 0;
+            while (c <= cLen-3){
+
+                for (int i = r; i < r+3;i++){
+                    for (int j = c;j < c+3;j++){
+                        System.out.print(board[i][j] + " ");
+                        if (board[i][j] != '.' && board[i][j]>'0' && board[i][j] <= '9' && threeMap.getOrDefault(Integer.valueOf(board[i][j]),0) > 0){
+                            return false;
+                        }
+                        threeMap.put(Integer.valueOf(board[i][j]), threeMap.getOrDefault(Integer.valueOf(board[i][j]),0)+1);
+                    }
+                    System.out.println();
+                }
+                c += 3;
+                threeMap.clear();
+                System.out.println();
+            }
+            r += 3;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] exampleBoard ={
+                {'7','.','.','.','4','.','.','.','.'},
+                {'.','.','.','8','6','5','.','.','.'},
+                {'.','1','.','2','.','.','.','.','.'},
+                {'.','.','.','.','.','9','.','.','.'},
+                {'.','.','.','.','5','.','5','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','2','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'}
+        };
+
+        isValidSudoku(exampleBoard);
+    }
+}
