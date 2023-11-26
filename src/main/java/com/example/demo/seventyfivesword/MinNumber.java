ae1c133debb80f284799421165bfa8ebfe853bc7
qinlida <qinlida@znlh.com>
2023-11-26 22:24:27 +0800

    [算法小练习]把数组排成最小的数

--- src/main/java/com/example/demo/seventyfivesword/MinNumber.java
+++ src/main/java/com/example/demo/seventyfivesword/MinNumber.java
@@ -20,12 +20,37 @@ public class MinNumber {
      * 输入: [3,30,34,5,9]
      * 输出: "3033459"
      */
+    //比较器：Goods[] arr = new Goods[6];
+    //        arr[0] = new Goods("lenovoMouse",34);
+    //        arr[1] = new Goods("dellMouse",43);
+    //        arr[2] = new Goods("xiaomiMouse",12);
+    //        arr[3] = new Goods("huaweiMouse",65);
+    //        arr[4] = new Goods("huaweiMouse",224);
+    //        arr[5] = new Goods("microsoftMouse",43);
+    //
+    //        Arrays.sort(arr, new Comparator() {
+    //            //指明商品比较大小的方式:按照产品名称从低到高排序,再按照价格从高到低排序
+    //            @Override
+    //            public int compare(Object o1, Object o2) {
+    //                if(o1 instanceof Goods && o2 instanceof Goods){
+    //                    Goods g1 = (Goods)o1;
+    //                    Goods g2 = (Goods)o2;
+    //                    if(g1.getName().equals(g2.getName())){
+    //                        return -Double.compare(g1.getPrice(),g2.getPrice());
+    //                    }else{
+    //                        return g1.getName().compareTo(g2.getName());
+    //                    }
+    //                }
+    //                throw new RuntimeException("输入的数据类型不一致");
+    //            }
+    //        });
+    //————————————————
     public String minNumber(int[] nums) {
         String[] strs = new String[nums.length];
         for (int i = 0; i < nums.length; i ++){
             strs[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(strs);
+        Arrays.sort(strs,(x,y)->(x+y).compareTo(y+x));
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < nums.length; i ++){
             sb.append(strs[i]);
