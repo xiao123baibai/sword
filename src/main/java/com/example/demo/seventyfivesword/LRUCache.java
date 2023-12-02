c0c6d80dbf2a07d21a95f73e27783925ebbd0df4
qinlida <qinlida@znlh.com>
2023-12-02 20:02:07 +0800

    [算法小练习]LRU (最近最少使用) 缓存

---
+++ src/main/java/com/example/demo/seventyfivesword/LRUCache.java
@@ -0,0 +1,140 @@
+package com.example.demo.seventyfivesword;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ *  LRU (最近最少使用) 缓存
+ */
+public class LRUCache extends LinkedHashMap<Integer,Integer> {
+    /**
+     * 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+     * 实现 LRUCache 类：
+     * LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+     * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+     * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+     * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+     *
+     *
+     *
+     * 示例：
+     *
+     * 输入
+     * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+     * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+     * 输出
+     * [null, null, null, 1, null, -1, null, -1, 3, 4]
+     *
+     * 解释
+     * LRUCache lRUCache = new LRUCache(2);
+     * lRUCache.put(1, 1); // 缓存是 {1=1}
+     * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+     * lRUCache.get(1);    // 返回 1
+     * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+     * lRUCache.get(2);    // 返回 -1 (未找到)
+     * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+     * lRUCache.get(1);    // 返回 -1 (未找到)
+     * lRUCache.get(3);    // 返回 3
+     * lRUCache.get(4);    // 返回 4
+     *
+     *
+     * 提示：
+     *
+     * 1 <= capacity <= 3000
+     * 0 <= key <= 10000
+     * 0 <= value <= 105
+     * 最多调用 2 * 105 次 get 和 put
+     */
+    private Map<Integer,DLinkedNode> cache = new HashMap<>();
+    private int size;
+
+    private int capacity;
+
+    private DLinkedNode head,tail;
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        tail = new DLinkedNode();
+        head = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null){
+            return -1;
+        }
+        //如果存在，则最近使用，移动到头部
+        moveHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node == null){
+            DLinkedNode newNode = new DLinkedNode(key,value);
+            cache.put(key,newNode);
+            size++;
+            addHead(newNode);
+            //超过缓存的最大数量
+            if (size > capacity){
+                DLinkedNode tail = removeTail();
+                cache.remove(tail.key);
+                size--;
+            }
+        }else {
+            node.value = value;
+            moveHead(node);
+        }
+    }
+
+    //头部添加
+    private void addHead(DLinkedNode node){
+        node.prev = head;
+        head.next = node;
+        head.next.prev = node;
+        node.next = head.next;
+    }
+
+    //移除节点
+    private void removeNode(DLinkedNode node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    //移除头部节点
+    private void moveHead(DLinkedNode node){
+        removeNode(node);
+        addHead(node);
+    }
+
+    private DLinkedNode removeTail(){
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    class DLinkedNode {
+        int key;
+        int value;
+
+        DLinkedNode prev;
+        DLinkedNode next;
+
+        public DLinkedNode(){}
+
+        public DLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    /**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+}
