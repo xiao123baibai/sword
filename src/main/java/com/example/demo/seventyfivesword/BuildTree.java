3008db6257cec071c0bac3ffdf1b4cb9c0c59426
qinlida <qinlida@znlh.com>
2023-12-03 22:03:10 +0800

    [算法小练习]重建二叉树--复习

--- src/main/java/com/example/demo/seventyfivesword/BuildTree.java
+++ src/main/java/com/example/demo/seventyfivesword/BuildTree.java
@@ -26,9 +26,31 @@ public class BuildTree {
      * Output: [-1]
      */
     public TreeNode buildTree2(int[] preorder, int[] inorder){
-
+        if (preorder == null || inorder == null){
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.push(root);
+        int inorderindex = 0;
+        for (int i = 1; i < preorder.length; i++){
+            int preorderval = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderindex]){
+                node.left = new TreeNode(preorder[i]);
+                stack.push(node.left);
+            }else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderindex]){
+                    node = stack.pop();
+                    inorderindex++;
+                }
+                node.right = new TreeNode(preorder[i]);
+                stack.push(node.right);
+            }
+        }
+        return root;
     }
-    //战歌走起：前后定根，中分左右
+    //战歌走起：前后定根，中分左右--先构建左节点，然后构建右节点
     public TreeNode buildTree1(int[] preorder, int[] inorder) {
         if (preorder == null || preorder.length == 0){
             return null;
@@ -93,7 +115,7 @@ public class BuildTree {
         //通过前序确定根结点
         TreeNode node = new TreeNode(preOrder[rootIndex]);
         int i = inMap.get(node.val);
-        //i代表的是根结点在inorder中的值
+        //i代表的是根结点在inorder中的值 ：根结点是从前序中取
         node.left = recur(rootIndex+1,leftIndex,i-1);
         node.right = recur(rootIndex + i-leftIndex+1,i+1,rightIndex);
         return node;
