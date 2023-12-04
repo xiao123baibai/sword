4a003b0bbc4c1c6a8b6dfbc079b61057981bb0be
qinlida <qinlida@znlh.com>
2023-12-04 17:08:36 +0800

    [算法小练习]剪绳子--复习

--- src/main/java/com/example/demo/seventyfivesword/CuttingRope.java
+++ src/main/java/com/example/demo/seventyfivesword/CuttingRope.java
@@ -37,4 +37,18 @@ public class CuttingRope {
             return (int) Math.pow(3,a)*2;
         }
     }
+
+    public int cuttingRope1(int n){
+        if (n <= 3){
+            return n-1;
+        }
+        int a = n/3,b = n%3;
+        if (b == 0){
+            return (int)Math.pow(3,a);
+        }else if (b == 1){
+            return (int)Math.pow(3,a-1)*4;
+        }else {
+            return (int)Math.pow(3,a)*2;
+        }
+    }
 }
