477740c8b1f06f301d1f6dc07669659b36a31f2b
qinlida <qinlida@znlh.com>
2023-12-05 23:11:21 +0800

    [算法小练习]数组中出现次数超过一半的数字--摩尔算法--复习

--- src/main/java/com/example/demo/seventyfivesword/MajorityElement.java
+++ src/main/java/com/example/demo/seventyfivesword/MajorityElement.java
@@ -61,4 +61,22 @@ public class MajorityElement {
         }
         return count > nums.length/2 ? x:0;
     }
+
+    //摩尔算法
+
+    public int majorityElement3(int[] nums){
+        int x = 0,votes = 0,count = 0;
+        for (int num : nums){
+            if (votes == 0){
+                x = num;
+            }
+            votes += (x == num ? 1:-1);
+        }
+        for (int num : nums){
+            if (x == num){
+                count++;
+            }
+        }
+        return count > nums.length/2 ? x:0;
+    }
 }
