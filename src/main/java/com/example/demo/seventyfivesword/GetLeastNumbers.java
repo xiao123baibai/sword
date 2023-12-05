6f921d7899850e41f404f14e14927c93797a8bc2
qinlida <qinlida@znlh.com>
2023-12-05 15:35:22 +0800

    [算法小练习]最小的k个数--快排--复习

--- src/main/java/com/example/demo/seventyfivesword/GetLeastNumbers.java
+++ src/main/java/com/example/demo/seventyfivesword/GetLeastNumbers.java
@@ -7,11 +7,35 @@ import java.util.Arrays;
  */
 public class GetLeastNumbers {
     public int[] getLeastNumbers(int[] arr, int k) {
-        Arrays.sort(arr);
+        arr = sort(arr,0,arr.length-1);
         int[] ans = new int[k];
         for(int i = 0; i < arr.length; i++){
             ans[i] = arr[i];
         }
         return ans;
+    }
+    //快排
+
+    public int[] sort(int[] arr,int left, int right){
+        int low = left;
+        int high = right;
+        if (low >= high){
+            return arr;
+        }
+        int piovt = arr[low];
+        while (low < high){
+            while (low < high && piovt <= arr[high]){
+                high--;
+            }
+            arr[low] = arr[high];
+            while (low < high && piovt >= arr[low]){
+                low++;
+            }
+            arr[high] = arr[low];
+        }
+        arr[low] = piovt;
+        sort(arr,left,low-1);
+        sort(arr,low+1,right);
+        return arr;
     }
 }
