31d6f33702ec26c08f6f882736e9bbf1746ebf0a
qinlida <qinlida@znlh.com>
2023-12-06 10:47:44 +0800

    [算法小练习]合并两个排序的列表--复习

--- src/main/java/com/example/demo/seventyfivesword/MergeTwoLists.java
+++ src/main/java/com/example/demo/seventyfivesword/MergeTwoLists.java
@@ -78,6 +78,43 @@ public class MergeTwoLists {
         }
         return cur;
     }
+    //遍历
+    public ListNode mergeTwoList4(ListNode l1, ListNode l2){
+        if (l1 == null){
+            return l2;
+        }
+        if (l2 == null){
+            return l1;
+        }
+        ListNode res = null;
+        if (l1.val <= l2.val){
+            res = l1;
+            l1 = l1.next;
+        }else {
+            res = l2;
+            l2 = l2.next;
+        }
+        ListNode cur = res;
+        while (l1 != null || l2 != null){
+            if (l1.val >= l2.val){
+                res.next = l2;
+                l2 = l2.next;
+                res = res.next;
+            }else {
+                res.next = l1;
+                l1 = l1.next;
+                res = res.next;
+            }
+        }
+        //尾节点
+        if (l1 == null){
+            res.next = l2;
+        }
+        if (l2 == null){
+            res.next = l1;
+        }
+        return cur;
+    }
     //递归
 
     /**
@@ -100,6 +137,21 @@ public class MergeTwoLists {
             return l1;
         } else {
             l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
+
+    //递归
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2){
+        if (l1 == null){
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val == l2.val) {
+            l1.next = mergeTwoLists3(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists3(l1,l2.next);
             return l2;
         }
     }
