60b567687b1650f7b0e25137038972e98bf45e4d
qinlida <qinlida@znlh.com>
2023-12-06 09:38:26 +0800

    [算法小练习]连续子数组的最大和--复习

--- src/main/java/com/example/demo/seventyfivesword/MaxSubArray.java
+++ src/main/java/com/example/demo/seventyfivesword/MaxSubArray.java
@@ -29,4 +29,13 @@ public class MaxSubArray {
         }
         return max;
     }
+
+    public int maxSubArray1(int[] nums){
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            nums[i] += Math.max(nums[i-1],0);
+            max = Math.max(nums[i],max);
+        }
+        return max;
+    }
 }
