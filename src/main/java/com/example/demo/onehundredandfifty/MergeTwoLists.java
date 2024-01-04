230b8ddad140d95dee3cb71c7de869dfd38e6e9f
qinlida <qinlida@znlh.com>
2024-01-04 14:49:41 +0800

    [面试经典150题]2. 两数相加

---
+++ src/main/java/com/example/demo/onehundredandfifty/MergeTwoLists.java
@@ -0,0 +1,106 @@
+package com.example.demo.onehundredandfifty;
+
+/**
+ * 21.合并两个有序数组
+ */
+public class MergeTwoLists {
+    /**
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：l1 = [1,2,4], l2 = [1,3,4]
+     * 输出：[1,1,2,3,4,4]
+     * 示例 2：
+     *
+     * 输入：l1 = [], l2 = []
+     * 输出：[]
+     * 示例 3：
+     *
+     * 输入：l1 = [], l2 = [0]
+     * 输出：[0]
+     *
+     *
+     * 提示：
+     *
+     * 两个链表的节点数目范围是 [0, 50]
+     * -100 <= Node.val <= 100
+     * l1 和 l2 均按 非递减顺序 排列
+     */
+    //迭代思路清晰，有待于使用链表的特点优化
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        ListNode head = null;
+        ListNode tail = null;
+        if (list2 == null && list1 != null){
+            head = tail = new ListNode(list1.val);
+            list1 = list1.next;
+        }
+        if (list1 == null && list2 != null){
+            head = tail = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        if (list1 != null && list2 != null){
+            if (list1.val >= list2.val){
+                head = tail = new ListNode(list2.val);
+                list2 = list2.next;
+            }else {
+                head = tail = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+        }
+        while (list1 != null || list2 != null){
+            if (list2 == null && list1 != null){
+                tail.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+            if (list1 == null && list2 != null){
+                tail.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            if (list1 != null && list2 != null){
+                if (list1.val >= list2.val){
+                    tail.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }else {
+                    tail.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+            }
+            tail = tail.next;
+        }
+        return head;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        if (list1 == null){
+            return list1;
+        }else if (list2 == null){
+            return list2;
+        } else if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next,list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1,list2.next);
+            return list2;
+        }
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
