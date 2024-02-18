500e64667e3c4b7cc70d868784f517e4a465bc34
qinlida <qinlida@znlh.com>
2024-02-18 20:44:37 +0800

    [剑指offer75题]4. 寻找两个正序数组的中位数

--- src/main/java/com/example/demo/ser/mediumNum.java
+++ src/main/java/com/example/demo/ser/mediumNum.java
@@ -2,7 +2,7 @@ package com.example.demo.ser;
 
 
 /**
- * 功能描述:
+ * 4. 寻找两个正序数组的中位数
  *
  * @Author: qinlida
  * @Date: 2022/8/29
@@ -270,5 +270,49 @@ public class mediumNum {
             }
         }
         return 0.0;
+    }
+
+    public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+        int length1 = nums1.length, length2 = nums2.length;
+        int totalLength = length1 + length2;
+        if(totalLength % 2 == 1){
+            int midIndex = totalLength / 2;
+            double median = getKthElement(nums1, nums2, midIndex + 1);
+            return median;
+        }else{
+            int midindex1 = totalLength / 2 - 1, midindex2 = totalLength / 2;
+            double median = (getKthElement(nums1, nums2, midindex1 + 1) + getKthElement(nums1, nums2, midindex2 + 1)) / 2.0;
+            return median;
+        }
+    }
+
+    public int getKthElement(int[] nums1, int[] nums2, int k){
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = 0, index2 = 0;
+        int kthElement = 0;
+        while(true){
+            if(index1 == length1){
+                return nums2[index2 + k -1];
+            }
+            if(index2 == length2){
+                return nums1[index1 + k - 1];
+            }
+            if(k == 1){
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+            //正常情况下
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, length1) - 1;
+            int newIndex2 = Math.min(index2 + half, length2) - 1;
+            int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+            if(pivot1 <= pivot2){
+                //剪枝
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            }else{
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
+        }
     }
 }
